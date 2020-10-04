c8c2d959a070d570b26dcbb75d6fbe76ac4611dd
SergoDobro <optimgrey@yandex.ru>
2020-10-04 13:50:45 +0300

    negTests

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTests.cs
@@ -5,16 +5,16 @@ namespace MathCalculatorTests
 {
     public class MathCalculatorTest
     {
+        const int Five = 5;
+        const int Three = 3;
         [Fact]
         public void WhenSumFiveAndThreeThaenGotEight()
         {
             // Arrange
-            const int Five = 5;
-            const int Three = 3;
             MathCalculatorr calculator = new MathCalculatorr();
 
             // Act
-            var result = calculator.Sum(5,3);
+            var result = calculator.Sum(Five,Three);
 
             // Assert
             Assert.Equal(8, result);
@@ -25,15 +25,39 @@ namespace MathCalculatorTests
         public void WhenSubFiveAndThreeThaenGotEight()
         {
             // Arrange
-            const int Five = 5;
-            const int Three = 3;
             MathCalculatorr calculator = new MathCalculatorr();
 
             // Act
-            var result = calculator.Substract(5,3);
+            var result = calculator.Substract(Five,Three);
 
             // Assert
             Assert.Equal(2, result);
+        
+        }
+        [Fact]
+        public void WhenNegSumFiveAndThreeThaenGotEight()
+        {
+            // Arrange
+            MathCalculatorr calculator = new MathCalculatorr();
+
+            // Act
+            var result = calculator.Sum(-Five,-Three);
+
+            // Assert
+            Assert.Equal(-8, result);
+        
+        }
+        [Fact]
+        public void WhenNegSubFiveAndThreeThaenGotEight()
+        {
+            // Arrange
+            MathCalculatorr calculator = new MathCalculatorr();
+
+            // Act
+            var result = calculator.Substract(-Five,Three);
+
+            // Assert
+            Assert.Equal(-8, result);
         
         }
 
