33f2c8f9ddd3599caff1b2f07adb64c1dc0930e4
SergoDobro <optimgrey@yandex.ru>
2020-10-18 12:15:42 +0300

    RSP1

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -1,8 +1,25 @@
 ﻿using System;
 
+
 namespace RockScissorsPaperLib
 {
+    public enum RSP { Rock, Scissors, Paper}
+    public enum Result { winner1, winner2, draw}
     public class WinnerChecker
     {
+        public Result Fight(RSP player1,RSP player2){
+            if (player1 == player2)
+            {
+                return Result.draw;
+            }
+            if (player1 == RSP.Paper )
+            {
+            
+            }
+        }
     }
+
+    
+
+
 }
