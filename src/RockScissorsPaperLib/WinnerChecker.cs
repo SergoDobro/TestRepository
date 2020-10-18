1f2a9db941685b814f430d9b4ef4909df11175d2
Stranik <rund2504@gmail.com>
2020-10-18 12:23:30 +0300

    RSP 2

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 
-
 namespace RockScissorsPaperLib
 {
-    public enum RSP { Rock, Scissors, Paper}
-    public enum Result { winner1, winner2, draw}
+    public enum RSP { Rock, Scissors, Paper }
+    public enum Result { Winner1, Winner2, Draw }
     public class WinnerChecker
     {
-        public Result Fight(RSP player1,RSP player2){
+        public Result Fight(RSP player1, RSP player2)
+        {
             if (player1 == player2)
             {
-                return Result.draw;
+                return Result.Draw;
             }
-            if (player1 == RSP.Paper )
+
+            if ((player1 == RSP.Paper && player2 == RSP.Rock) || (player1 == RSP.Rock && player2 == RSP.Scissors) || (player1 == RSP.Scissors && player2 == RSP.Paper))
             {
-            
+                return Result.Winner1;
             }
+
+            return Result.Winner2;
         }
     }
 
--- src/RockScissorsPaperTests/WinnerCheckerTests.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTests.cs
@@ -9,7 +9,66 @@ namespace RockScissorsPaperTests
         [Fact]
         public void Test1()
         {
+            // Arrange
             WinnerChecker winnerChecker = new WinnerChecker();
+
+            // Act
+            var result = winnerChecker.Fight(RSP.Paper, RSP.Rock);
+
+            // Assert
+            Assert.Equal(Result.Winner1, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            WinnerChecker winnerChecker = new WinnerChecker();
+
+            // Act
+            var result = winnerChecker.Fight(RSP.Paper, RSP.Paper);
+
+            // Assert
+            Assert.Equal(Result.Draw, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            WinnerChecker winnerChecker = new WinnerChecker();
+
+            // Act
+            var result = winnerChecker.Fight(RSP.Scissors, RSP.Rock);
+
+            // Assert
+            Assert.Equal(Result.Winner2, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            // Arrange
+            WinnerChecker winnerChecker = new WinnerChecker();
+
+            // Act
+            var result = winnerChecker.Fight(RSP.Paper, RSP.Scissors);
+
+            // Assert
+            Assert.Equal(Result.Winner2, result);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            // Arrange
+            WinnerChecker winnerChecker = new WinnerChecker();
+
+            // Act
+            var result = winnerChecker.Fight(RSP.Rock, RSP.Rock);
+
+            // Assert
+            Assert.Equal(Result.Draw, result);
         }
     }
 }
